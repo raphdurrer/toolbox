b17df6f28bce48c1706feb547f52530c12f13313
Raphael <raphael@docono.io>
2022-12-16 11:17:54 +0100

    updating index with options, snipptes, sections

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 
 Kirby::plugin('raphdurrer/toolbox', [
-
-    'blueprints' =>[
-        'sections/address' => __DIR__. '/src/blueprints/sections/address.yml',
-        'sections/socialmedia' => __DIR__. '/src/blueprints/sections/socialmedia.yml'
+    'options' => [
+        'siteurl' => site()->url()
     ],
-    'snippets' =>[
-        'address' => __DIR__. '/src/snippets/address.php',
-        'socialmedia' => __DIR__. '/src/snippets/socialmedia.php'
+    'blueprints' => [
+        'sections/address' => __DIR__ . '/src/blueprints/sections/address.yml',
+        'sections/socialmedia' => __DIR__ . '/src/blueprints/sections/socialmedia.yml'
+    ],
+    'snippets' => [
+        'address' => __DIR__ . '/src/snippets/address.php',
+        'socialmedia' => __DIR__ . '/src/snippets/socialmedia.php'
     ],
 ]);
