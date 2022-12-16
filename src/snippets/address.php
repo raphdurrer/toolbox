0404678ee8f155d18d7a799e88837334ef68b78d
Raphael <raphael@docono.io>
2022-12-16 11:18:25 +0100

    adding address blueprint

--- src/snippets/address.php
+++ src/snippets/address.php
@@ -1,1 +1,11 @@
-Adresse
+<?php if ($contact = $data->contact()->toObject()) : ?>
+    <address class="address" role="contentinfo">
+        <?php e($contact->name(), $contact->name()) ?>
+        <?php e($contact->address(), $contact->address()) ?>
+        <?php e($contact->plz(), $contact->plz()) ?>
+        <?php e($contact->place(), $contact->place()) ?>
+        <?php e($contact->email(), $contact->email()) ?>
+        <?php e($contact->tel(), $contact->tel()) ?>
+        <?php e($contact->website(), $contact->website()) ?>
+    </address>
+<?php endif; ?>
