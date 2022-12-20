e311aae2e89c7b1e8305303df2fe182111774f0c
Raphael <raphael@docono.io>
2022-12-20 11:51:58 +0100

    bugfixing pageconten
    Bugfixing option in blueprint and adding grid colcount and gridgap css var to snippet

--- src/snippets/pagecontent.php
+++ src/snippets/pagecontent.php
@@ -5,16 +5,15 @@
         $properties =  $layout->layoutProperties()->toObject();
         $layoutId = $properties->layoutid();
         $layoutClasses = $properties->layoutClasses();
+        $grid = $layout->gridproperties()->toObject();
         ?>
 
-        <section <?php e($layoutId->isNotEmpty(), "id='" . $layoutId . "'"); ?> <?php e($layoutClasses->isNotEmpty(), "class='" . str_replace(",", "", $layoutClasses) . "'") ?> style="--blockStart:<?= $properties->layoutBlockStart() ?>; --blockEnd:<?= $properties->layoutBlockEnd() ?>">
-            <div class="row" role="row">
-                <?php foreach ($layout->columns() as $column) : ?>
-                    <div class="column" style="--columns:<?= $column->span(12) ?>">
-                        <?= $column->blocks() ?>
-                    </div>
-                <?php endforeach; ?>
-            </div>
+        <section role="row" <?php e($layoutId->isNotEmpty(), "id='" . $layoutId . "'"); ?> class="layout <?php e($layoutClasses->isNotEmpty(),  str_replace(",", "", $layoutClasses)) ?>" style="--colCount:<?= $grid->colcount() ?>; --gridGap:<?= $grid->gridgap() ?>; --blockStart:<?= $properties->layoutBlockStart() ?>; --blockEnd:<?= $properties->layoutBlockEnd() ?>">
+            <?php foreach ($layout->columns() as $column) : ?>
+                <div class="column" style="--columns:<?= $column->span(12) ?>">
+                    <?= $column->blocks() ?>
+                </div>
+            <?php endforeach; ?>
         </section>
     <?php endforeach; ?>
 <?php endif; ?>
