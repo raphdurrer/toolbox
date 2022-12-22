2381f1392d293d64391ed131f7a3235d75c65716
Raphael <raphael@docono.io>
2022-12-22 15:23:44 +0100

    remove menus from plugin
    menus are so different to each site that is why it is no longer part of the plugin

--- index.php
+++ index.php
@@ -4,26 +4,26 @@ use Kirby\Cms\Url;
 
 Kirby::plugin('raphdurrer/toolbox', [
     'options' => [
+        // Options
         'siteUrl' => Url::home(),
         'jsonDir' => __DIR__ . '/assets/jsonData',
-
     ],
     'blueprints' => [
-        'tabs/menu' => __DIR__ . '/src/blueprints/tabs/menu.yml',
+        // Tabs
         'tabs/pagefiles' => __DIR__ . '/src/blueprints/tabs/pagefiles.yml',
         'tabs/seo' => __DIR__ . '/src/blueprints/tabs/seo.yml',
-
+        // Sections
         'sections/pagecontent' => __DIR__ . '/src/blueprints/sections/pagecontent.yml',
         'sections/address' => __DIR__ . '/src/blueprints/sections/address.yml',
         'sections/socialmedia' => __DIR__ . '/src/blueprints/sections/socialmedia.yml',
-
+        // Files
         'files/logoimage' => __DIR__ . '/src/blueprints/files/logoimage.yml',
-
+        // User
         'users/editor' => __DIR__ . '/src/blueprints/users/editor.yml'
     ],
     'snippets' => [
+        // Snippets
         'logo' => __DIR__ . '/src/snippets/logo.php',
-        'mainmenu' => __DIR__ . '/src/snippets/mainmenu.php',
         'pagecontent' => __DIR__ . '/src/snippets/pagecontent.php',
         'address' => __DIR__ . '/src/snippets/address.php',
         'socialmedia' => __DIR__ . '/src/snippets/socialmedia.php'
